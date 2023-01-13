d116744c350159e8c625b780df6e5c944d624bf3
Marc Rickenbach <marc.rickenbach@gmail.com>
2023-01-13 15:34:02 -0800

    Cleaned up comments

--- main.c
+++ main.c
@@ -113,7 +113,8 @@ int main(void) {
 	adc_init();
 	dma_channel1_init();
 	i2c_init();
-	dma_config((uint32_t)&ADC1->DR, (uint32_t)adc_buffer, 4);	// configure and start the DMA from ADC
+    // configure and start the DMA from ADC
+	dma_config((uint32_t)&ADC1->DR, (uint32_t)adc_buffer, 4);	
 
 	configure_light_sensor ();
 
@@ -132,31 +133,48 @@ int main(void) {
 
 
 void clk_init(void) {	// checked, ok
-	RCC->CR 		|= 	(1U<<0);		// Enable HSI_16
-	while (!(RCC->CR & 	(1U<<2))){};	// Wait for HSI_RDY Flag, then we can keep going
-	RCC->APB1ENR 	|= 	(1U<<28);		// Set the Power Enable CLock
-	PWR->CR 		|= 	(1U<<11);		// Set the VOS, Voltage Scaling Range, to Range 1 (01), bits 12:11.
+	// Enable HSI_16
+    RCC->CR 		|= 	(1U<<0);
+    // Wait for HSI_RDY Flag, then we can keep going
+	while (!(RCC->CR & 	(1U<<2))){};
+    // Set the Power Enable CLock
+	RCC->APB1ENR 	|= 	(1U<<28);
+    // Set the VOS, Voltage Scaling Range, to Range 1 (01), bits 12:11.
+	PWR->CR 		|= 	(1U<<11);
 	PWR->CR 		&= 	~(1U<<12);
-	RCC->APB2ENR 	|= 	(1U<<0);		// Set SYSCFG Enable
-	FLASH->ACR		= 	0x40;			// PreRead is Enabled (bit 6)
-	RCC->CFGR 		|= 	(1U<<7);		// AHB, divided by 2[1000]
-	RCC->CFGR 		&= 	~(1U<<10);		// APB1 not divided, 0xx bits 10-8
-	RCC->CFGR 		&= 	~(1U<<13);		// APB2 not divided, 0xx bits 13-11
-	RCC->CFGR 		|= 	(1U<<18);		// Set PLL Multiplication factor to 4 [0001] in bits 21:18
-	RCC->CFGR 		|= 	(1U<<22);		// Set PLL Division factor to 2 [01] in bits 23:22
-	RCC->CR 		|= 	(1U<<24);		// RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	while (!(RCC->CR & (1U<<25))){};	// Wait for PLL_RDY Flag, then continue on.
-	RCC->CFGR 		|= 	(1U<<0);		// Select the PLL as SYSCLK Source, [11]in bits 1:0
+    // Set SYSCFG Enable
+	RCC->APB2ENR 	|= 	(1U<<0);
+    // PreRead is Enabled (bit 6)
+	FLASH->ACR		= 	0x40;
+    // AHB, divided by 2[1000]
+	RCC->CFGR 		|= 	(1U<<7);
+    // APB1 not divided, 0xx bits 10-8
+	RCC->CFGR 		&= 	~(1U<<10);
+    // APB2 not divided, 0xx bits 13-11
+	RCC->CFGR 		&= 	~(1U<<13);
+    // Set PLL Multiplication factor to 4 [0001] in bits 21:18
+	RCC->CFGR 		|= 	(1U<<18);
+    // Set PLL Division factor to 2 [01] in bits 23:22
+	RCC->CFGR 		|= 	(1U<<22);
+    // RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC->CR 		|= 	(1U<<24);
+    // Wait for PLL_RDY Flag, then continue on.
+	while (!(RCC->CR & (1U<<25))){};
+    // Select the PLL as SYSCLK Source, [11]in bits 1:0
+	RCC->CFGR 		|= 	(1U<<0);
 	RCC->CFGR 		|= 	(1U<<1);
-	while (!(RCC->CFGR & (3U<<2))){};	// Wait for Status Flag to read PLL [11] in bits 3:2
-	RCC->APB1ENR |= (1U<<21);			// Enable I2C1 Clock
+    // Wait for Status Flag to read PLL [11] in bits 3:2
+	while (!(RCC->CFGR & (3U<<2))){};
+    // Enable I2C1 Clock
+	RCC->APB1ENR |= (1U<<21);
 
 }
 
 
 
 void gpio_init(void) {	// checked, ok
-	RCC->IOPENR 	|= 	(1U<<0);				// Enable GPIO A and B Clocks
+    // Enable GPIO A and B Clocks
+	RCC->IOPENR 	|= 	(1U<<0);
 	RCC->IOPENR 	|= 	(1U<<1);
 	GPIOA->MODER 	= 	0xebeb54ff;
 	GPIOA->OTYPER 	= 	0x600;
@@ -173,74 +191,113 @@ void gpio_init(void) {	// checked, ok
 
 
 void tim2_init(void){ // checked, ok
-	RCC->APB1ENR	|= 	(1U<<0);			// Enable Clock Access to TIM 2
-	TIM2->PSC 		= 	100;				// Set Pre-Scaler Value
-	TIM2->ARR 		= 	100;				// Set Auto-Reload Value
-	TIM2->CR1 		&= 	~(1U<<4);			// Set Direction, 0 is up-counter
-	TIM2->CR1 		|= 	(1U<<7);			// Auto-reload preload enable
-	TIM2->CNT 		= 	0;					// Clear Counter
-	TIM2->CR2 		|= 	(1U<<5);			// Sets Timer to update event on trigger output
-	TIM2->CR1 		|= 	(1U<<0);			// Enable Timer
-	TIM2->DIER 		|= 	(1U<<0);			// Enable TIM Interrupt
-	NVIC_EnableIRQ(TIM2_IRQn);				// Enable TIM Interrupt in NVIC
+// Enable Clock Access to TIM 2
+	RCC->APB1ENR |= (1U<<0);
+    // Set Pre-Scaler Value	
+	TIM2->PSC = 100;
+    // Set Auto-Reload Value
+	TIM2->ARR = 100;
+    // Set Direction, 0 is up-counter
+	TIM2->CR1 &= ~(1U<<4);
+    // Auto-reload preload enable
+	TIM2->CR1 |= (1U<<7);
+    // Clear Counter
+	TIM2->CNT = 0;
+    // Sets Timer to update event on trigger output
+	TIM2->CR2 |= (1U<<5);
+    // Enable Timer
+	TIM2->CR1 |= (1U<<0);
+    // Enable TIM Interrupt
+	TIM2->DIER |= (1U<<0);
+    // Enable TIM Interrupt in NVIC
+	NVIC_EnableIRQ(TIM2_IRQn);
 }
 
-
 void adc_init() {	//checked, ok, fixed channel select
-	RCC->APB2ENR 	|= 	(1U<<9);			//Enable Clock Access to ADC
-	ADC1->CHSELR 	= 	0xF; 				// ADC Channel Selection Register: ADC_CHSELR
-	ADC1->IER		|= 	(1U<<4);			// ADC Overrun interrupt enable
-	ADC1->CR 		|= 	(1U<<0);			// ADC Enable
-	ADC1->CFGR1 	= 	0x11483;	 		// ADC CONFIGURATION REGISTER 1: ADC1->CFGR1
-	ADC1->CFGR2 	&= 	~(1U<<0);			// Over-sampling Mode Disabled
-	ADC1->SMPR 		= 	0x7;				// ADC SAMPLING TIME REGISTER: ADC1->SMPR
-	ADC1->CR 		|= 	(1U<<2);			// Start ADC
+    //Enable Clock Access to ADC
+	RCC->APB2ENR |= (1U<<9);
+    // ADC Channel Selection Register: ADC_CHSELR
+	ADC1->CHSELR = 0xF;
+    // ADC Overrun interrupt enable
+	ADC1->IER |= (1U<<4);
+    // ADC Enable
+	ADC1->CR |= (1U<<0);
+    // ADC CONFIGURATION REGISTER 1: ADC1->CFGR1
+	ADC1->CFGR1 = 0x11483;
+    // Over-sampling Mode Disabled
+	ADC1->CFGR2 &= ~(1U<<0);
+    // ADC SAMPLING TIME REGISTER: ADC1->SMPR
+	ADC1->SMPR = 0x7;
+    // Start ADC
+	ADC1->CR |= (1U<<2);
 }
 
 
 void dma_channel1_init(void) {
-	RCC->AHBENR |= (1U<<0);				// Enable Clock Access to the DMA
-	DMA1_Channel1->CCR &= ~(1U<<0);		// Disable the DMA Channel 1, CxS 0000 so we can configure it
+    // Enable Clock Access to the DMA
+	RCC->AHBENR |= (1U<<0);
+    // Disable the DMA Channel 1, CxS 0000 so we can configure it
+	DMA1_Channel1->CCR &= ~(1U<<0);
 	while(DMA1_Channel1->CCR & (1U<<0)){}
-	DMA1->IFCR |= (1U<<0);				// Clear all interrupt flags for Channel 1, CxS 0000
-	DMA1_Channel1->CCR &= ~(1U<<12);	// Set Channel Priority to High
+    // Clear all interrupt flags for Channel 1, CxS 0000
+	DMA1->IFCR |= (1U<<0);
+    // Set Channel Priority to High
+	DMA1_Channel1->CCR &= ~(1U<<12);
 	DMA1_Channel1->CCR |= (1U<<13);
-	DMA1_Channel1->CCR &= ~(1U<<4);		// Set Data Transfer Direction, read from Peripheral (ADC)
-	DMA1_Channel1->CCR |= (1U<<5);		// Set Circular Mode
-	DMA1_Channel1->CCR &= ~(1U<<6);		// Set Peripheral Increment Mode to disabled
-	DMA1_Channel1->CCR |= (1U<<7);		// Set Memory Increment Mode to enabled
-	DMA1_Channel1->CCR &= ~(1U<<10);	// Set Memory Data Size, 32 bit
+    // Set Data Transfer Direction, read from Peripheral (ADC)
+	DMA1_Channel1->CCR &= ~(1U<<4);
+    // Set Circular Mode
+	DMA1_Channel1->CCR |= (1U<<5);
+    // Set Peripheral Increment Mode to disabled
+	DMA1_Channel1->CCR &= ~(1U<<6);
+    // Set Memory Increment Mode to enabled
+	DMA1_Channel1->CCR |= (1U<<7);
+    // Set Memory Data Size, 32 bit
+	DMA1_Channel1->CCR &= ~(1U<<10);
 	DMA1_Channel1->CCR |= (1U<<11);
-	DMA1_Channel1->CCR &= ~(1U<<8);		// Set Peripheral Size to 32 bit
+    // Set Peripheral Size to 32 bit
+	DMA1_Channel1->CCR &= ~(1U<<8);
 	DMA1_Channel1->CCR |= (1U<<9);
-	DMA1_Channel1->CCR |= (1U<<1);		// Enable Interrupted at Full Transfer, TCIE, enabled
+    // Enable Interrupted at Full Transfer, TCIE, enabled
+	DMA1_Channel1->CCR |= (1U<<1);
 	DMA1_Channel1->CCR |= (1U<<2);
 	DMA1_Channel1->CCR |= (1U<<3);
 }
 
 
 void i2c_init(void) {
-	GPIOA->OTYPER |= (1U<<9) | (1U<<10);	// Open Drain on I2C pins, PA9, PA10
-	GPIOA->OSPEEDR |= (3U<<18) | (3U<<20);	// High Speed, Pins PA9, PA10
-	GPIOA->PUPDR |= (1U<<18) | (1U<<20);	// Pull-Up on Pins PA9, PA10
-	RCC->APB1RSTR |= (1U<<21);			// Resets I2C1
-	RCC->APB1RSTR &= ~(1U<<21);			// Takes out of Reset State I2C1
-	I2C1->CR1 = 0;						// clear
+    // Open Drain on I2C pins, PA9, PA10
+	GPIOA->OTYPER |= (1U<<9) | (1U<<10);
+    // High Speed, Pins PA9, PA10
+	GPIOA->OSPEEDR |= (3U<<18) | (3U<<20);
+    // Pull-Up on Pins PA9, PA10
+	GPIOA->PUPDR |= (1U<<18) | (1U<<20);
+    // Resets I2C1
+	RCC->APB1RSTR |= (1U<<21);
+    // Takes out of Reset State I2C1
+	RCC->APB1RSTR &= ~(1U<<21);
+    // clear
+	I2C1->CR1 = 0;
 	I2C1->CR2 = 0;
-	I2C1->TIMINGR = 0x0010061A;			// Set Timing Register Properties
+    // Set Timing Register Properties
+	I2C1->TIMINGR = 0x0010061A;
 	I2C1->OAR1 = 0;
 	I2C1->OAR2 = 0;
-	I2C1->CR2 |=  (0x52<<1); 			// Set Target address
-	I2C1->CR1 &= ~(1U<<12); 			// Analog Filter Enable
-	I2C1->CR1 |= (0U<<8);				// Digital Filter Disable
-	I2C1->CR1 &= ~(1U<<17);				// Clock stretching disabled
-	I2C1->CR2 &= ~(1U<<11);				// ADD10 to 0 for 7-bit, 1 for 10-bit Addressing Mode
-	SYSCFG->CFGR2 |= (1U<<12);			// Fast Mode is Enabled
-	I2C1->CR1 |= (1U<<0);				// SET PE bit in I2C_CR1
+    // Set Target address
+	I2C1->CR2 |=  (0x52<<1);
+    // Analog Filter Enable
+	I2C1->CR1 &= ~(1U<<12);
+    // Digital Filter Disable
+	I2C1->CR1 |= (0U<<8);
+    // Clock stretching disabled
+	I2C1->CR1 &= ~(1U<<17);
+    // ADD10 to 0 for 7-bit, 1 for 10-bit Addressing Mode
+	I2C1->CR2 &= ~(1U<<11);
+    // Fast Mode is Enabled
+	SYSCFG->CFGR2 |= (1U<<12);
+    // SET PE bit in I2C_CR1
+	I2C1->CR1 |= (1U<<0);
 }
-
-
-
 
 
 //////////////////////////////////////////////////////////////////////
@@ -248,10 +305,14 @@ void i2c_init(void) {
 //////////////////////////////////////////////////////////////////////
 
 void dma_config(uint32_t src, uint32_t dst, uint32_t len) {
-	DMA1_Channel1->CPAR = src;			// Set source buffer
-	DMA1_Channel1->CMAR = dst;			// Set the destination buffer
-	DMA1_Channel1->CNDTR = len;			// Set the data length
-	DMA1_Channel1->CCR |= (1U<<0);		// Activate the Channel
+	// Set source buffer
+    DMA1_Channel1->CPAR = src;
+    // Set the destination buffer
+	DMA1_Channel1->CMAR = dst;
+    // Set the data length
+	DMA1_Channel1->CNDTR = len;
+    // Activate the Channel
+	DMA1_Channel1->CCR |= (1U<<0);
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -259,15 +320,18 @@ void dma_config(uint32_t src, uint32_t dst, uint32_t len) {
 //////////////////////////////////////////////////////////////////////
 
 void TIM2_IRQHandler(void) {
-	for (int i=0; i<4; i++) {		// Transfer our ADC readings from the DMA buffer to our local variables for use.
+    // Transfer our ADC readings from the DMA buffer to our local variables for use.
+	for (int i=0; i<4; i++) {
 		adc_value[i] = (adc_buffer[i] & 0xFFF)>>4;
 		if (adc_value[i] >= 253) adc_value[i] = 255;
 		if (adc_value[i] <= 20) adc_value[i] = 40;
 	}
 
-	flag_bank |= (1U<<0);			// ADC ready flag goes up so we can enter our main loop
-
-	TIM2->SR &= ~(1U<<0);			// Clear the Interrupt Flag
+    // ADC ready flag goes up so we can enter our main loop
+	flag_bank |= (1U<<0);
+    
+    // Clear the Interrupt Flag
+	TIM2->SR &= ~(1U<<0);			
 }
 
 
@@ -319,7 +383,8 @@ void configure_light_sensor () {
     	TEST |= (1U<<0);
     }
 
-    uint8_t datatowrite_meas = 0x22;    // Measurement and Rate Parameters
+    // Measurement and Rate Parameters
+    uint8_t datatowrite_meas = 0x22; 
     i2c_tx (LS_MEAS_RATE, &datatowrite_meas, 1);
     for (int i=0; i<10000; i++){};
     i2c_rx (LS_MEAS_RATE, &datacheck, 1);
@@ -327,7 +392,8 @@ void configure_light_sensor () {
     	TEST |= (1U<<1);
     }
 
-    uint8_t datatowrite_gain = 0x02;    // Gain setting
+    // Gain setting
+    uint8_t datatowrite_gain = 0x02;
     i2c_tx (LS_GAIN, &datatowrite_gain, 1);
     for (int i=0; i<10000; i++){};
     i2c_rx (LS_GAIN, &datacheck, 1);
@@ -362,16 +428,20 @@ void configure_light_sensor () {
 
 
 void read_sensor () {
-	i2c_rx (RED_REGISTER, red_val, 3); 	// RED
+    // RED
+	i2c_rx (RED_REGISTER, red_val, 3);
 	RED_TOTAL = (red_val[2] | red_val[1] | red_val[0])<<LIGHT_RESOLUTION;
 
-	i2c_rx (GREEN_REGISTER, green_val, 3);	// GREEN
+    // GREEN
+	i2c_rx (GREEN_REGISTER, green_val, 3);
 	GREEN_TOTAL = (green_val[2] | green_val[1] | green_val[0])<<LIGHT_RESOLUTION;
 
-	i2c_rx (BLUE_REGISTER, blue_val, 3);	// BLUE
+    // BLUE
+	i2c_rx (BLUE_REGISTER, blue_val, 3);
 	BLUE_TOTAL = (blue_val[2] | blue_val[1] | blue_val[0])<<LIGHT_RESOLUTION;
 
-	i2c_rx (IR_REGISTER, ir_val, 3);		// IR
+    // IR
+	i2c_rx (IR_REGISTER, ir_val, 3);
 	IR_TOTAL = (ir_val[2] | ir_val[1] | ir_val[0])<<LIGHT_RESOLUTION;
 }
 
@@ -387,20 +457,31 @@ void i2c_tx (uint8_t reg, uint8_t *TX_data, uint8_t size) {
 	uint8_t *pBufferData = TX_data;
 	uint8_t transferSize = size;
 
-	I2C1->CR1 &= ~I2C_CR1_PE; 										// Disable Peripheral
-	I2C1->CR2 &= ~(I2C_CR2_NBYTES); 								// Clear Nbytes
-	I2C1->CR2 |= ((transferSize+1)<<16);							// Set Nbytes to transfer size and one for register address
-	I2C1->CR2 &= ~(I2C_CR2_RD_WRN); 								// Set Write Request
-	I2C1->CR1 |= I2C_CR1_PE; 										// Enable Peripheral
-	I2C1->CR2 |= I2C_CR2_AUTOEND;									// Enable AutoEnd
-	I2C1->CR2 |= (1U<<13);											// Start
+    // Disable Peripheral
+	I2C1->CR1 &= ~I2C_CR1_PE; 		
+    // Clear Nbytes								
+	I2C1->CR2 &= ~(I2C_CR2_NBYTES);
+    // Set Nbytes to transfer size and one for register address
+	I2C1->CR2 |= ((transferSize+1)<<16);
+    // Set Write Request
+	I2C1->CR2 &= ~(I2C_CR2_RD_WRN);
+    // Enable Peripheral
+	I2C1->CR1 |= I2C_CR1_PE;
+    // Enable AutoEnd
+	I2C1->CR2 |= I2C_CR2_AUTOEND;
+    // Start
+	I2C1->CR2 |= (1U<<13);
 
-	while(!((I2C1->ISR & I2C_ISR_TXIS) == (I2C_ISR_TXIS))); 		// Wait for TX buffer to be empty
-	I2C1->TXDR = reg; 												// Send register address
+    // Wait for TX buffer to be empty
+	while(!((I2C1->ISR & I2C_ISR_TXIS) == (I2C_ISR_TXIS)));
+    // Send register address
+	I2C1->TXDR = reg;
 
     while(transferSize > 0) {
-    	while(!((I2C1->ISR & I2C_ISR_TXE) == (I2C_ISR_TXE))); 		// Wait for TX buffer to be empty
-    	I2C1->TXDR = *pBufferData; 									// Send bytes to Data Register
+        // Wait for TX buffer to be empty
+    	while(!((I2C1->ISR & I2C_ISR_TXE) == (I2C_ISR_TXE)));
+        // Send bytes to Data Register
+    	I2C1->TXDR = *pBufferData;
     	pBufferData++;
     	transferSize--;
     }
@@ -419,24 +500,37 @@ void i2c_rx (uint8_t reg, uint8_t *RX_data, uint8_t size) {
 	uint8_t *pBufferData = RX_data;
 	uint8_t transferSize = size;
 
-	I2C1->CR1 &= ~I2C_CR1_PE; 										// Disable Peripheral
-	I2C1->CR2 &= ~(I2C_CR2_NBYTES); 								// Clear Nbytes
-	I2C1->CR2 |= (1U<<16); 											// Set Nbytes to 1 for register address
-	I2C1->CR2 &= ~(I2C_CR2_RD_WRN); 								// Set Write Request
-	I2C1->CR1 |= I2C_CR1_PE; 										// Enable Peripheral
+    // Disable Peripheral
+	I2C1->CR1 &= ~I2C_CR1_PE;
+    // Clear Nbytes
+	I2C1->CR2 &= ~(I2C_CR2_NBYTES);
+    // Set Nbytes to 1 for register address
+	I2C1->CR2 |= (1U<<16);
+    // Set Write Request
+	I2C1->CR2 &= ~(I2C_CR2_RD_WRN);
+    // Enable Peripheral
+	I2C1->CR1 |= I2C_CR1_PE;
     I2C1->CR2 &= ~I2C_CR2_AUTOEND;
 
-	I2C1->CR2 |= (1U<<13);											// Start
+    // Start
+	I2C1->CR2 |= (1U<<13);
 
-	while(!((I2C1->ISR & I2C_ISR_TXIS) == (I2C_ISR_TXIS))); 		// Wait for TX buffer to be empty
-	I2C1->TXDR = reg; 												// Send register address
-	while(!((I2C1->ISR & I2C_ISR_TC) == (I2C_ISR_TC))); 			// Wait for transfer complete
+    // Wait for TX buffer to be empty
+	while(!((I2C1->ISR & I2C_ISR_TXIS) == (I2C_ISR_TXIS)));
+    // Send register address
+	I2C1->TXDR = reg;
+    // Wait for transfer complete
+	while(!((I2C1->ISR & I2C_ISR_TC) == (I2C_ISR_TC)));
 
-    I2C1->CR2 &= ~(I2C_CR2_NBYTES); 								// Clear Nbytes
-    I2C1->CR2 |= (transferSize)<<I2C_CR2_NBYTES_Pos; 				// Set Nbytes to transfer size
-    I2C1->CR2 |= (I2C_CR2_RD_WRN);									// Set Read Request
+    // Clear Nbytes
+    I2C1->CR2 &= ~(I2C_CR2_NBYTES);
+    // Set Nbytes to transfer size
+    I2C1->CR2 |= (transferSize)<<I2C_CR2_NBYTES_Pos;
+    // Set Read Request
+    I2C1->CR2 |= (I2C_CR2_RD_WRN);
 
-    I2C1->CR2 |= (1U<<13);											// Start
+    // Start
+    I2C1->CR2 |= (1U<<13);
 
     while (transferSize > 0) {
 
